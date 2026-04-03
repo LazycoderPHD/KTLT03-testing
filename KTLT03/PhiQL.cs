4f8b99b23a5df10ffee2f7774fd9ee4d42ef3ea2
LazycoderPHD <phatdao2309@gmail.com>
2026-04-04 04:12:05 +0700

    Add project files.

---
+++ KTLT03/PhiQL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTLT03
+{
+    public class PhiQL
+    {
+        protected string idQL;
+        protected int monthQL;
+        protected int yearQL;
+    }
+
+    public string 
+}
